530b45e90374e9c7138f6df34f2c094e5f800517
Andrew Rodionov <camelot58@gmail.com>
2018-03-01 05:14:37 -0800

    - добавлена структура, описывающая версию ПО, как major.minor.patch.

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,46 @@
-#include <iostream>
 #include "lib.h"
 
-int version()
+bool operator==(const version_info& lhs, const version_info& rhs) {
+  if ((lhs.major == rhs.major)
+    && (lhs.minor == rhs.minor)
+    && (lhs.patch == rhs.patch))
+    return true;
+  return false;
+}
+
+bool operator!=(const version_info& lhs, const version_info& rhs) {
+  return !(lhs == rhs);
+}
+
+bool operator<(const version_info& lhs, const version_info& rhs) {
+  if (lhs.major < rhs.major)
+    return true;
+  if (lhs.minor < rhs.minor)
+    return true;
+  if (lhs.patch < rhs.patch)
+    return true;
+  return false;
+}
+
+bool operator>(const version_info& lhs, const version_info& rhs) {
+  return rhs < lhs;
+}
+
+bool operator<=(const version_info& lhs, const version_info& rhs) {
+  return !(lhs > rhs);
+}
+
+bool operator>=(const version_info& lhs, const version_info& rhs) {
+  return !(lhs < rhs);
+}
+
+std::ostream& operator<<(std::ostream& out, const version_info& out_version) {
+  return out << out_version.major << '.' << out_version.minor << '.' << out_version.patch;
+}
+
+version_info version()
 {
-	return PROJECT_VERSION_PATCH;
+  return version_info(PROJECT_VERSION_MAJOR,
+                      PROJECT_VERSION_MINOR,
+                      PROJECT_VERSION_PATCH);
 }
--- test_main.cpp
+++ test_main.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_suite_main)
 
 BOOST_AUTO_TEST_CASE(test_version_valid)
 {
-	BOOST_CHECK( version() > 0 );
+	BOOST_CHECK( version() > version_info() );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
